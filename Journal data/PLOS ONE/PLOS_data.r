60977b11dd7e7ad288ad4c4cf531a26c3550bbb1
protohedgehog <jon.tennant.2gmail.com>
2018-04-26 16:14:59 +0800

    Added PLOS ONE content

--- Journal data/PLOS ONE/PLOS_data.r
+++ Journal data/PLOS ONE/PLOS_data.r
@@ -1,26 +1,22 @@
 # Using R to visualise Paleontology publications in PLOS ONE through time
 
-# Install package from within R
+# Install package from CRAN
 install.packages("rplos")
 
 # Load package from library
 library("rplos")
 
-# setwd("C:/Users/PC/Documents/GitHub")
-
 # Search for the term 'paleontology'
-plos_data<-searchplos('paleontology', 'id,publication_date', limit = 10000)
+PLOS_data<-searchplos('paleontology', fl=c('id','publication_date','author','copyright',
+                                           'counter_total_all','financial_disclosure','pagecount',
+                                           'title','volume'), limit = 10000)
 
 # Export file as a csv to your working directory
-write.csv(plos_data, file = "PLOS_data.csv")
-
+write.csv(PLOS_data, file = "PLOS_data.csv")
 
 # So there's a lot of data..
 # Let's try plotting it!
 
 # Plot results through time, no limit on data
 plot_throughtime(terms='paleontology',limit=10000)
-
-# You can try this with any field really
-plot_throughtime(terms='paleontology',limit=3000)
 
