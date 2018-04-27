75a5825c063dacb817636859473cb16e78ffa83a
protohedgehog <jon.tennant.2gmail.com>
2018-04-27 17:20:13 +0800

    Citation analysis for APP

---
+++ Journal data extract.r
@@ -0,0 +1,38 @@
+# Pull in relevant data from Journal data source files
+# Replace with local directory where needed
+
+# Acta Palaeontologica Polonica
+# Note that some lines are still corrupted here, even after parsing. These were eliminated to avoid data errors (file version 2)
+APP_scopus<-read.csv("C:/Users/PC/Documents/GitHub/OpenPaleo/Journal data/Acta Palaeontologica Polonica/ActaPalaeontologicaPolonica_Scopus2.csv")
+APP_unpaywall<-read.csv("C:/Users/PC/Documents/GitHub/OpenPaleo/Journal data/Acta Palaeontologica Polonica/ActaPalaeontologicaPolonica_Unpaywall.csv")
+
+# Create a new factor for OA status
+APP_OA<-APP_unpaywall[,"is_oa"]
+
+APP_DOI<-APP_scopus[,"DOI"]
+
+# Pull out citation counts, and replace NAs with zero counts
+APP_cite<-APP_scopus[,"Cited.by"]
+APP_cite[is.na(APP_cite)]<-0
+
+# Histogram plot
+APP_hist<-hist(APP_cite,col="red",xlab="Citation count",
+               main="Acta Palaeontologica Polonica",breaks=20)
+
+# Add a normal distribution curve
+xfit<-seq(min(APP_cite),max(APP_cite),length=10) 
+yfit<-dnorm(xfit,mean=mean(APP_cite),sd=sd(APP_cite)) 
+yfit <- yfit*diff(APP_hist$mids[1:2])*length(APP_cite)
+lines(xfit,yfit,col="blue",lwd=2)
+
+# Try a density plot instead
+plot(density(APP_cite),xlab="Citation count",
+     main="Acta Palaeontologica Polonica")
+polygon((density(APP_cite)),col = "red")
+
+
+# Summarise how many articles are OA and how many are not
+summary(APP_OA)
+
+# Plot as a proportional barplot
+barplot(prop.table(table(APP_OA)),main="Acta Palaeontologica Polonica, OA proportion")
